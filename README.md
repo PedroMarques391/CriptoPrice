4570c7889bf94a25d71cf9edd56489d90ec97a70
Pedro <pedromarques391@gmail.com>
2023-09-21 10:09:59 -0300

    Finalização

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # CriptoPrice
 
-Neste projeto, fizemos uso da API do Mercado Bitcoin, que pode ser acessada em https://api.mercadobitcoin.net/api/v4/docs. Desenvolvemos o projeto utilizando JavaScript e aplicamos estilos com o framework Tailwind CSS. Inicialmente, adotamos a abordagem 'mobile first', o que significa que a interface foi projetada para dispositivos móveis em primeiro lugar. No entanto, ainda estamos trabalhando na formatação final e na estilização do projeto para dispositivos desktop.
+Neste projeto, fizemos uso da API do Mercado Bitcoin, que pode ser acessada em https://api.mercadobitcoin.net/api/v4/docs. Desenvolvi o projeto utilizando JavaScript e aplicamos estilos com o framework Tailwind CSS. Inicialmente, adotei a abordagem 'mobile first', o que significa que a interface foi projetada para dispositivos móveis em primeiro lugar..
 
 O principal objetivo do projeto era consumir a API do Mercado Bitcoin e fornecer os dados solicitados aos usuários. Uma das etapas cruciais envolve a formatação dos resultados da requisição para garantir que os dados sejam apresentados de maneira clara e legível.
-
-Embora o projeto ainda não esteja totalmente concluído, estamos trabalhando ativamente para aprimorar a experiência do usuário, estilizar a interface para desktop e garantir que todas as funcionalidades estejam funcionando conforme o planejado.
--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,45 @@
 const coins = document.getElementById('coin');
+
 async function getAllTicker(coin = 'BTC') {
+    try {
     const URL = `https://www.mercadobitcoin.net/api/${coin}/ticker/`
     const response = await fetch(URL)
     const data = await response.json()
     console.log(data)
-
     const itens = document.getElementById('itensLi');
 
-    itens.innerHTML = `
-    <p class='text-center'>Moeda Selecionada: ${coins.value}</p>
-    <ul class="text-justify">
-            <li class="p-2">Alta: R$${parseFloat(data.ticker.high)}</li>
-            <li class="p-2">Baixa: R$${parseFloat(data.ticker.low)}</li>
-            <li class="p-2">Volume: R$${parseFloat(data.ticker.vol)}</li>
-            <li class="p-2">Última: R$${parseFloat(data.ticker.last)}</li>
-            <li class="p-2">Compra: R$${data.ticker.buy}</li>
-            <li class="p-2">Venda: R$${parseFloat(data.ticker.sell)}</li>
-            <li class="p-2">Abertura: R$${parseFloat(data.ticker.open)}</li>
-            <li class="p-2">Data: ${data.ticker.date}</li>
-    </ul>
-    `
-}
+    
+    //Função para formatar valores
+    function formataValores(valor) {
+        if(valor != data.ticker.date) {
+            return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(valor)
+        } 
+    }
+    //Função para formatar data
+    const dataConsulta = new Date(data.ticker.date * 1000)
+    const dataFormatada = dataConsulta.toLocaleString()
 
+    itens.innerHTML = `
+    <p class='text-center md:text-2xl md:text-yellow-400 md:font-bold'>Moeda Selecionada: ${coins.value}</p>
+          <ul class="text-justify md:text-center md:text-2xl md:mt-8 md:font-serif md:border border-yellow-400 md:shadow-2xl md:shadow-yellow-400">
+                  <li class="p-2 leading-15">Alta: R$${formataValores(data.ticker.high)}</li>
+                  <li class="p-2 leading-15">Baixa: R$${formataValores(data.ticker.low)}</li>
+                  <li class="p-2 leading-15">Última: R$${formataValores(data.ticker.last)}</li>
+                  <li class="p-2 leading-15">Compra: R$${formataValores(data.ticker.buy)}</li>
+                  <li class="p-2 leading-15">Venda: R$${formataValores(data.ticker.sell)}</li>
+                  <li class="p-2 leading-15">Abertura: R$${formataValores(data.ticker.open)}</li>
+                  <li class="p-2 leading-15">Data: ${dataFormatada}</li>
+          </ul>
+    `} catch(error) {
+        const itens = document.getElementById('itensLi');
+        itens.innerHTML = `<p class='text-center text-2xl text-red-600 font-bold border border-red-600 shadow-2xl shadow-red-600'>Você precisa selecionar a moeda desejada para continuar.</p>`
+    }
+    
+    
+}
 
 const btn = document.getElementById('price');
 
